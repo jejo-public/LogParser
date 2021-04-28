5b7befa480b4ef97e999b3c78e735cbc3f56a1b6
jens.johannes <jens.johannes@ebcsoft.de>
2021-04-28 13:48:57 +0200

    DataParser filter function added

--- src/main/java/DataParser.java
+++ src/main/java/DataParser.java
@@ -1,3 +1,28 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class DataParser {
 
+  String filterForSystem;
+  List<String> filterForComponents;
+
+  public DataParser(final String filterForSystem, final List<String> filterForComponents) {
+    this.filterForSystem = filterForSystem;
+    this.filterForComponents = filterForComponents;
+  }
+
+  public List<Map<String, String>> filter(final List<Map<String, String>> dataSets) {
+    return dataSets.stream().filter(this::filterCondition).collect(Collectors.toList());
+  }
+
+  private boolean filterCondition(final Map<String, String> dataSet) {
+    if (Boolean.parseBoolean(dataSet.get(DataReader.LOG_FIELD_IS_EXPIRED))) {
+      return false;
+    }
+    if (!dataSet.get(DataReader.LOG_FIELD_SYSTEM).equals(filterForSystem)) {
+      return false;
+    }
+    return filterForComponents.contains(dataSet.get(DataReader.LOG_FIELD_COMPONENT));
+  }
 }
--- src/main/java/LogParser.java
+++ src/main/java/LogParser.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class LogParser {
 
 
-  public static void main(String[] args) {
-    List<Map<String, String>>  loadedDataSets =  new DataReader().readData();
+  public static void main(final String[] args) {
+    final List<Map<String, String>> loadedDataSets = new DataReader().readData();
 
-    System.out.println(loadedDataSets);
-    for(int i = 0; i < args.length; i++) {
-      System.out.println(args[i]);
+    final List<String> components = new ArrayList<>();
+    components.add("TEXT");
+    components.add("HIPM");
+
+    final List<Map<String, String>> filteredDataSets = new DataParser("PROD", components)
+        .filter(loadedDataSets);
+
+    System.out.println(filteredDataSets);
+    for (final String arg : args) {
+      System.out.println(arg);
     }
   }
 
